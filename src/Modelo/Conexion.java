482c89c2eb32c818ee837806e9dccbef5ac41bd5
WFJACC <wfjacc.blz@gmail.com>
2023-03-27 16:25:49 -0600

    Se agrego la carpeta lib, ademas del conector de sql, modificaciones en Conexion.java y app.java

--- src/Modelo/Conexion.java
+++ src/Modelo/Conexion.java
@@ -1,12 +1,13 @@
 package Modelo;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-
 public class Conexion {
     static Connection conn = null;
-    static String URL = "jdbc:mysql://localhost:3306/ticketdb";
+    static String base = "ticketdb";
+    static String URL = "jdbc:mysql://localhost:3306/" + base;
     static String USER = "root";
     static String PASSWORD = "";
 
@@ -15,9 +16,9 @@ public class Conexion {
             Class.forName("com.mysql.cj.jdbc.Driver");
             conn = DriverManager.getConnection(URL, USER, PASSWORD);
             System.out.println("Conexión establecida.");
-        } catch (ClassNotFoundException |SQLException e) {
+        } catch (ClassNotFoundException | SQLException e) {
             System.err.println(e);
-            e.printStackTrace();
+
         }
         return conn;
     }
--- src/mvc/App.java
+++ src/mvc/App.java
@@ -1,17 +1,17 @@
 package mvc;
+
 import Controladores.CtrlInicio;
 import Modelo.Conexion;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-
 public class App {
-        public static void main(String[] args) throws Exception {
-            CtrlInicio ctrlInicio = new CtrlInicio();
-            ctrlInicio.iniciar();
+    public static void main(String[] args) throws Exception {
+        CtrlInicio ctrlInicio = new CtrlInicio();
+        ctrlInicio.iniciar();
 
         Connection conn = Conexion.obtenerConexion();
-        if(conn != null) {
+        if (conn != null) {
             System.out.println("Conexion exitosa");
         } else {
             System.out.println("No se pudo hacer la conexión");
