7e692cd83331949aa84c5b9729e62007727e9852
WFJACC <wfjacc.blz@gmail.com>
2023-03-28 00:28:19 -0600

    Cambios en el metodo agregar de ConsultasAlumno.java

--- src/Modelo/ConsultasAlumno.java
+++ src/Modelo/ConsultasAlumno.java
@@ -1,112 +1,108 @@
 package Modelo;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class ConsultasAlumno extends Conexion{
-    public boolean registrar(Alumno pro){
+public class ConsultasAlumno extends Conexion {
+    public boolean registrar(Alumno pro, Municipio mp) {
         PreparedStatement ps = null;
         Connection conn = obtenerConexion();
-        String sql= "INSERT INTO alumno (nombre,Paterno,materno,telefono,correo,nivel,asunto) values (?,?,?,?,?,?,?)";
-        try{
-            ps= conn.prepareStatement(sql);
-            ps.setString(1,pro.getNombre());
-            ps.setString(2,pro.getPaterno());
-            ps.setString(3,pro.getMaterno());
-            ps.setString(4,pro.getTelefono());
-            ps.setString(5,pro.getCorreo());
-            ps.setString(6,pro.getNivel());
-            ps.setString(7,pro.getAsunto());
+        String sql = "INSERT INTO alumno (curp,nombre,Paterno,materno,telefono,correo,nivel,asunto,municipio.idmunicipio)"
+                +
+                "select ?,?,?,?,?,?,?,?, municipio.idmunicipio from municipio where municipio.nombre = ?";
+        try {
+            ps = conn.prepareStatement(sql);
+            ps.setString(1, pro.getCurp());
+            ps.setString(2, pro.getNombre());
+            ps.setString(3, pro.getPaterno());
+            ps.setString(4, pro.getMaterno());
+            ps.setString(5, pro.getTelefono());
+            ps.setString(6, pro.getCorreo());
+            ps.setString(7, pro.getNivel());
+            ps.setString(8, pro.getAsunto());
+            ps.setString(8, mp.getNombre());
             ps.execute();
             return true;
-        }
-        catch(SQLException e){
+        } catch (SQLException e) {
             System.err.println(e);
             return false;
-        } 
-        finally{
-            try{
+        } finally {
+            try {
                 conn.close();
-            }
-            catch(SQLException e){
+            } catch (SQLException e) {
                 System.err.println(e);
-            
+
             }
         }
-        
-    }//end registrar
-    
-    public boolean modificar(Alumno pro){
+
+    }// end registrar
+
+    public boolean modificar(Alumno pro) {
         PreparedStatement ps = null;
         Connection conn = obtenerConexion();
-        String sql= "UPDATE producto SET nombre=?,Paterno=?,materno=?,telefono=?,correo=?,nivel=?,asunto=? where curp=?";
-        try{
-            ps= conn.prepareStatement(sql);
-            ps.setString(1,pro.getNombre());
-            ps.setString(2,pro.getPaterno());
-            ps.setString(3,pro.getMaterno());
-            ps.setString(4,pro.getTelefono());
-            ps.setString(5,pro.getCorreo());
-            ps.setString(6,pro.getNivel());
-            ps.setString(7,pro.getAsunto());
-            ps.setString(9,pro.getCurp());
+        String sql = "UPDATE producto SET nombre=?,Paterno=?,materno=?,telefono=?,correo=?,nivel=?,asunto=? where curp=?";
+        try {
+            ps = conn.prepareStatement(sql);
+            ps.setString(1, pro.getNombre());
+            ps.setString(2, pro.getPaterno());
+            ps.setString(3, pro.getMaterno());
+            ps.setString(4, pro.getTelefono());
+            ps.setString(5, pro.getCorreo());
+            ps.setString(6, pro.getNivel());
+            ps.setString(7, pro.getAsunto());
+            ps.setString(9, pro.getCurp());
             ps.execute();
             return true;
-        }
-        catch(SQLException e){
+        } catch (SQLException e) {
             System.err.println(e);
             return false;
-        } 
-        finally{
-            try{
+        } finally {
+            try {
                 conn.close();
-            }
-            catch(SQLException e){
+            } catch (SQLException e) {
                 System.err.println(e);
-            
+
             }
         }
-        
-    }//end modificar
-    
-    public boolean eliminar(Alumno pro){
+
+    }// end modificar
+
+    public boolean eliminar(Alumno pro) {
         PreparedStatement ps = null;
         Connection con = obtenerConexion();
-        String sql= "DELETE FROM alumno WHERE curp=?";
-        try{
-            ps= con.prepareStatement(sql);
-            ps.setString(1,pro.getCurp());
+        String sql = "DELETE FROM alumno WHERE curp=?";
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setString(1, pro.getCurp());
             ps.execute();
             return true;
-        }
-        catch(SQLException e){
+        } catch (SQLException e) {
             System.err.println(e);
             return false;
-        } 
-        finally{
-            try{
+        } finally {
+            try {
                 con.close();
-            }
-            catch(SQLException e){
+            } catch (SQLException e) {
                 System.err.println(e);
-            
+
             }
         }
-        
-    }//end eliminar
-    
-    public boolean buscar(Alumno pro){
+
+    }// end eliminar
+
+    public boolean buscar(Alumno pro) {
         PreparedStatement ps = null;
         ResultSet rs = null;
         Connection con = obtenerConexion();
-        String sql= "SELECT * FROM alumno WHERE curp=?";
-        try{
-            ps= con.prepareStatement(sql);
-            ps.setString(1,pro.getCurp());
-            rs=ps.executeQuery();
-            
-            if (rs.next()){
+        String sql = "SELECT * FROM alumno WHERE curp=?";
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setString(1, pro.getCurp());
+            rs = ps.executeQuery();
+
+            if (rs.next()) {
                 pro.setCurp(rs.getString("curp"));
                 pro.setNombre(rs.getString("nombre"));
                 pro.setPaterno(rs.getString("Paterno"));
@@ -115,25 +111,22 @@ public class ConsultasAlumno extends Conexion{
                 pro.setCorreo(rs.getString("correo"));
                 pro.setNivel(rs.getString("nivel"));
                 pro.setAsunto(rs.getString("asunto"));
-                return true;  
+                return true;
             }
-                    
+
             return false;
-        }
-        catch(SQLException e){
+        } catch (SQLException e) {
             System.err.println(e);
             return false;
-        } 
-        finally{
-            try{
+        } finally {
+            try {
                 conn.close();
-            }
-            catch(SQLException e){
+            } catch (SQLException e) {
                 System.err.println(e);
-            
+
             }
         }
-        
-    }//end buscar
-    
- }//end class
+
+    }// end buscar
+
+}// end class
