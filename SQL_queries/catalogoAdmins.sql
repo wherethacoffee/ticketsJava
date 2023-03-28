26a2f738d04ef42c673ae73b24d6356b7c0d44f8
wherethacoffee <whr.thacoff23@gmail.com>
2023-03-27 23:58:33 -0600

    Se ha agregado el catalogo de admins

---
+++ SQL_queries/catalogoAdmins.sql
@@ -0,0 +1,5 @@
+INSERT INTO `ticketsdb`.`admin` (`usuario`, `contrasena`) VALUES ('jorgeadmin', 'admin123');
+INSERT INTO `ticketsdb`.`admin` (`usuario`, `contrasena`) VALUES ('juanadmin', 'admin321');
+INSERT INTO `ticketsdb`.`admin` (`usuario`, `contrasena`) VALUES ('jaccadmin', 'admin123');
+INSERT INTO `ticketsdb`.`admin` (`usuario`, `contrasena`) VALUES ('edsonadmin', 'admin321');
+
