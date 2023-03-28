76d704a2d866ce671bb558815c305c88bb3fa7b3
NoSenseQC <edsonarmijoruiz@gmail.com>
2023-03-27 19:50:51 -0600

    clase admin y municipio del modelo

---
+++ src/Modelo/Admin.java
@@ -0,0 +1,31 @@
+package Modelo;
+
+public class Admin {
+    private int idusuario;
+    private String usuario;
+    private String contraseña;
+
+    public int getIdusuario() {
+        return idusuario;
+    }
+
+    public void setIdusuario(int idusuario) {
+        this.idusuario = idusuario;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+}
---
+++ src/Modelo/Municipio.java
@@ -0,0 +1,22 @@
+package Modelo;
+
+public class Municipio {
+    private int idmunicipio;
+    private String nombre;
+
+    public int getIdmunicipio() {
+        return idmunicipio;
+    }
+
+    public void setIdmunicipio(int idmunicipio) {
+        this.idmunicipio = idmunicipio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
