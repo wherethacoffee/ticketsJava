0dece1fc9cdfacb4ec56de06ee56307bab15d4f4
wherethacoffee <whr.thacoff23@gmail.com>
2023-03-28 00:58:01 -0600

    Se le han agregado los metodos getters y setters a la clase

---
+++ src/Modelo/MunicipioTurno.java
@@ -0,0 +1,23 @@
+package Modelo;
+
+public class MunicipioTurno {
+    private int idmunicipio;
+    private int nturno;
+
+    public int getIdmunicipio() {
+        return idmunicipio;
+    }
+
+    public void setIdmunicipio(int idmunicipio) {
+        this.idmunicipio = idmunicipio;
+    }
+    
+    public int getNturno() {
+        return nturno;
+    }
+    
+    public void setNturno(int nturno) {
+        this.nturno = nturno;
+    }
+
+}
