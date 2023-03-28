81e8d93529f7b40bed804ade4870416fd3bffc4c
NoSenseQC <edsonarmijoruiz@gmail.com>
2023-03-27 19:40:28 -0600

    clase Alumno de modelo

--- src/Modelo/Alumno.java
+++ src/Modelo/Alumno.java
@@ -4,4 +4,92 @@ package Modelo;
 public class Alumno {
     private String curp;
     private String nombre;
+    private String Paterno;
+    private String materno;
+    private String telefono;
+    private String correo;
+    private String nivel;
+    private String asunto;
+    private int municipio_idmunicipio;
+    private String status;  
+
+    public String getCurp() {
+        return curp;
+    }
+
+    public void setCurp(String curp) {
+        this.curp = curp;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPaterno() {
+        return Paterno;
+    }
+
+    public void setPaterno(String Paterno) {
+        this.Paterno = Paterno;
+    }
+
+    public String getMaterno() {
+        return materno;
+    }
+
+    public void setMaterno(String materno) {
+        this.materno = materno;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(String nivel) {
+        this.nivel = nivel;
+    }
+
+    public String getAsunto() {
+        return asunto;
+    }
+
+    public void setAsunto(String asunto) {
+        this.asunto = asunto;
+    }
+
+    public int getMunicipio_idmunicipio() {
+        return municipio_idmunicipio;
+    }
+
+    public void setMunicipio_idmunicipio(int municipio_idmunicipio) {
+        this.municipio_idmunicipio = municipio_idmunicipio;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
