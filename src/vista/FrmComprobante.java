fc3681482dda8e3c0a965835cee1ada273c9fded
NoSenseQC <edsonarmijoruiz@gmail.com>
2023-03-29 23:25:48 -0600

    frame Comprobante

---
+++ src/vista/FrmComprobante.java
@@ -0,0 +1,90 @@
+package vista;
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+
+
+public class FrmComprobante extends JFrame {
+
+    public JPanel contentPane;
+
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    FrmComprobante frame = new FrmComprobante();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /**
+     * Create the frame.
+     */
+    public FrmComprobante() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 450, 460);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+        setContentPane(contentPane);
+        contentPane.setLayout(null);
+
+        JPanel panel = new JPanel();
+        panel.setBounds(10, 10, 450, 460);
+        contentPane.add(panel);
+        panel.setLayout(null);
+
+
+        JLabel lblComprobante = new JLabel("Comprobante");
+        lblComprobante.setFont(new Font("Times New Roman", Font.PLAIN, 30));
+        lblComprobante.setBounds(110, 30, 200, 30);
+        panel.add(lblComprobante);
+        
+        
+        JLabel lblNombre = new JLabel("Nombre:");
+        lblNombre.setFont(new Font("Times New Roman", Font.PLAIN, 20));
+        lblNombre.setBounds(35, 120, 102, 24);
+        panel.add(lblNombre);
+
+        JLabel lblNombreVacio = new JLabel("");
+        lblNombreVacio.setFont(new Font("Times New Roman", Font.PLAIN, 19));
+        lblNombreVacio.setBounds(115, 120, 250, 24);
+        panel.add(lblNombreVacio);
+
+        JLabel lblAsunto = new JLabel("Asunto:");
+        lblAsunto.setFont(new Font("Times New Roman", Font.PLAIN, 20));
+        lblAsunto.setBounds(42, 200, 102, 24);
+        panel.add(lblAsunto);
+
+        JLabel lblAsuntoVacio = new JLabel("");
+        lblAsuntoVacio.setFont(new Font("Times New Roman", Font.PLAIN, 20));
+        lblAsuntoVacio.setBounds(115, 200, 300, 24);
+        panel.add(lblAsuntoVacio);
+
+        JLabel lblTurno = new JLabel("Tu turno es:");
+        lblTurno.setFont(new Font("Times New Roman", Font.PLAIN, 20));
+        lblTurno.setBounds(15, 280, 102, 24);
+        panel.add(lblTurno);
+
+        JLabel lblTurnoVacio = new JLabel("");
+        lblTurnoVacio.setFont(new Font("Times New Roman", Font.PLAIN, 20));
+        lblTurnoVacio.setBounds(115, 280, 180, 24);
+        panel.add(lblTurnoVacio);
+
+
+    }
+
+
+    
+}
