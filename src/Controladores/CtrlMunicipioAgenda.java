393b9e66371ed15d4173d282b3c242baab340e7d
wherethacoffee <whr.thacoff23@gmail.com>
2023-03-31 17:57:55 -0600

    Se le agregaron los metodos faltantes al controlador

--- src/Controladores/CtrlMunicipioAgenda.java
+++ src/Controladores/CtrlMunicipioAgenda.java
@@ -3,6 +3,8 @@ package Controladores;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JOptionPane;
+
 import Modelo.ConsultasMunicipio;
 import Modelo.Municipio;
 import vista.FrmGestorMunicipio;
@@ -33,25 +35,60 @@ public class CtrlMunicipioAgenda implements ActionListener {
     public void actionPerformed(ActionEvent e) {
 
         if (e.getSource() == vista.btnAgregar){
+            modelo.setIdmunicipio(Integer.parseInt(vista.txtIDMunicipio.getText()));
+            modelo.setNombre(vista.txtNombreMunicipio.getText());
 
+            if (consultas.registrar(modelo)){
+                JOptionPane.showMessageDialog(null, "Municipio Registrado con exito");
+                limpiar();
+            } else {
+                JOptionPane.showMessageDialog(null, "Municipio no registrado");
+                limpiar();
+            }
         }
 
         if (e.getSource() == vista.btnEliminar){
+            modelo.setIdmunicipio(Integer.parseInt(vista.txtIDMunicipio.getText()));
 
+            if (consultas.eliminar(modelo)){
+                JOptionPane.showMessageDialog(null, "Municipio Eliminado con exito");
+                limpiar();
+            } else {
+                JOptionPane.showMessageDialog(null, "Municipio no eliminado");
+                limpiar();
+            }
         }
 
         if (e.getSource() == vista.btnActualizar){
+            modelo.setIdmunicipio(Integer.parseInt(vista.txtIDMunicipio.getText()));
+            modelo.setNombre(vista.txtNombreMunicipio.getText());
 
+            if (consultas.modificar(modelo)){
+                JOptionPane.showMessageDialog(null, "Municipio modificado con exito");
+                limpiar();
+            } else {
+                JOptionPane.showMessageDialog(null, "Municipio no actualizado");
+                limpiar();
+            }
+        }
+
+        if (e.getSource() == vista.btnBuscar){
+            modelo.setIdmunicipio(Integer.parseInt(vista.txtIDMunicipio.getText()));
+
+            if (consultas.buscar(modelo)){
+                vista.txtIDMunicipio.setText(String.valueOf(modelo.getIdmunicipio()));
+                vista.txtNombreMunicipio.setText(modelo.getNombre());
+                limpiar();
+            } else {
+                JOptionPane.showMessageDialog(null, "Municipio no encontrado");
+                limpiar();
+            }
         }
 
         if (e.getSource() == vista.btnSalir){
-
+            System.exit(0);
         }
         
-        if (e.getSource() == vista.btnBuscar){
-
-        }
-
         if (e.getSource() == vista.btnLimpiar){
             limpiar();
         }
